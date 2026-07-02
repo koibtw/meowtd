33bd0b93f6a8b11d294bcb9b16a85aa0a3fa61da
june <me@koi.rip>
2026-07-02 15:13:58 +0200

    release: 0.3.0

--- nix/packages/receive.nix
+++ nix/packages/receive.nix
@@ -6,7 +6,7 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "meowtd-receive";
-  version = "0.1.0";
+  version = "0.3.0";
   src = ../..;
 
   nativeBuildInputs = [
--- nix/packages/send.nix
+++ nix/packages/send.nix
@@ -8,7 +8,7 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "meowtd";
-  version = "0.1.0";
+  version = "0.3.0";
   src = ../..;
 
   buildInputs = [ libssh2 ];
